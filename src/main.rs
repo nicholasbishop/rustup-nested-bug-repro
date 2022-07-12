21615720d5f9e9d1ae182ec04e84f810903d1eca
Nicholas Bishop <nbishop@nbishop.net>
2022-07-12 14:59:22 -0400

    Run "cargo +nightly version"

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use std::process::Command;
+
 fn main() {
-    println!("Hello, world!");
+    let mut cmd = Command::new("cargo");
+    cmd.env_remove("RUSTC");
+    cmd.env_remove("RUSTDOC");
+    cmd.args(&["+nightly", "version"]);
+
+    println!("running command: {:?}", cmd);
+    let s = cmd.status().unwrap();
+    assert!(s.success());
 }
