e27a76aaa182eb77846fa73c5e310223c0a4792b
Nicholas Bishop <nbishop@nbishop.net>
2022-07-12 15:08:13 -0400

    Add option to modify the path

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,25 @@
 use std::process::Command;
+use std::env;
 
 fn main() {
+    // Modify the path if any argument is passed in.
+    let modify_path = env::args().len() == 2;
+
     let mut cmd = Command::new("cargo");
     cmd.env_remove("RUSTC");
     cmd.env_remove("RUSTDOC");
     cmd.args(&["+nightly", "version"]);
+
+    if modify_path {
+        let orig_path = env::var_os("PATH").unwrap_or_default();
+        let modified_split_paths = env::split_paths(&orig_path).filter(|path| {
+            !path
+                .components()
+                .any(|component| component.as_os_str() == ".rustup")
+        });
+        let modified_path = env::join_paths(modified_split_paths).expect("invalid PATH");
+        cmd.env("PATH", modified_path);
+    }
 
     println!("running command: {:?}", cmd);
     let s = cmd.status().unwrap();
